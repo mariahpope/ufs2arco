9f216f7480c9ddfbf6b8339c9f0ec01b51d5e3b3
Mariah Pope <mariah.pope@tomorrow.io>
2024-04-01 12:26:17 -0400

    change memory test

--- examples/replay/subsample/test_memory.py
+++ examples/replay/subsample/test_memory.py
@@ -3,10 +3,9 @@ import sys
 
 ds = xr.open_zarr("gcs://noaa-ufs-gefsv13replay/ufs-hr1/0.25-degree/03h-freq/zarr/fv3.zarr",
                   storage_options={"token": "anon"},)
-ds = ds.isel(time=slice(1,2), 
+ds = ds.isel(time=slice(1,3), 
              grid_xt=slice(None, None, 4), 
-             grid_yt=slice(None, None, 4))
-ds = ds.load()
+             grid_yt=slice(None, None, 4)).load()
 print(ds)
 
 print(sys.getsizeof(ds))
