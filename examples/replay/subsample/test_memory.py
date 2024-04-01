c00b72f1336ff8118f3cbe393f0568fde42fa4a1
Mariah Pope <mariah.pope@tomorrow.io>
2024-04-01 12:44:07 -0400

    memory test update

--- examples/replay/subsample/test_memory.py
+++ examples/replay/subsample/test_memory.py
@@ -3,7 +3,7 @@ import sys
 
 ds = xr.open_zarr("gcs://noaa-ufs-gefsv13replay/ufs-hr1/0.25-degree/03h-freq/zarr/fv3.zarr",
                   storage_options={"token": "anon"},)
-ds = ds.isel(time=slice(1,2), 
+ds = ds.isel(time=slice(0,128), 
              grid_xt=slice(None, None, 4), 
              grid_yt=slice(None, None, 4)).load()
 print(ds)
