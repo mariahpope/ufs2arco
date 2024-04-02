19e35b90b1ba8619ec48e615f88f47c4eba75bf1
Mariah Pope <mariah.pope@tomorrow.io>
2024-04-02 13:51:12 -0400

    loading spot change

--- examples/replay/subsample/subsampler.py
+++ examples/replay/subsample/subsampler.py
@@ -10,7 +10,7 @@ ds = xr.open_zarr("gcs://noaa-ufs-gefsv13replay/ufs-hr1/0.25-degree/03h-freq/zar
 start_timestep = int(sys.argv[1])
 end_timestep = int(sys.argv[2]) + 1
 timestep_total = end_timestep - start_timestep
-timestep_per_loop = 15
+timestep_per_loop = 18
 groups = int(timestep_total/timestep_per_loop)
 splits = [int(x) for x in np.linspace(start_timestep, end_timestep, groups+1)]
 
@@ -19,10 +19,9 @@ for i in range(len(splits) - 1):
     start_time = splits[i]
     end_time = splits[i + 1]
     
-    ds_subset = ds.isel(time=slice(start_time,end_time)).load()
-
-    ds_subset = ds_subset.isel(grid_xt=slice(None, None, 4), 
-                               grid_yt=slice(None, None, 4))
+    ds_subset = ds.isel(time=slice(start_time,end_time),
+                        grid_xt=slice(None, None, 4), 
+                        grid_yt=slice(None, None, 4)).load()
     
     ds_subset = ds_subset.chunk({"time":1, 
                                  "pfull":127, 
